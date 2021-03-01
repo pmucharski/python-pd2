03c3e633c1e126f620ecf59d0c18c6912af4318c
pmucharski <pmucharski2k@gmail.com>
2021-03-01 12:08:13 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+#zad.1
+from typing import Dict
+
+lista = ['piłka nożna', 'siatkówka', 'koszykówka', 'hokej', 'łyżwiarstwo']
+print(lista)
+
+lista.reverse()
+print(lista)
+
+lista.append('rugby')
+lista.append('baseball')
+print(lista)
+
+#zad.2
+slownik = {'af':'afery', 'nw':'news', 'pop':'popularne', 'fm':'fame mma'}
+print(slownik)
+
+#zad.3
+slownik1 = {'lol':'league of legends', 'cs':'counter strike', 'wow':'world of tanks'}
+print(slownik1)
+print(len(slownik1))
+
+#zad.4
+napis = input('Wprowadz zdanie:')
+print(napis)
+print(napis.count('a'))
+
+#zad.5
+
